338a7c5f39b96ff8993481dd7ae7a869cc3aa160
Igor <greafdorothy@yandex.com>
2024-10-30 21:55:17 +0300

    add hw3

--- Homework/03/alibi.py
+++ Homework/03/alibi.py
@@ -15,9 +15,18 @@ def compute_alibi(num_heads: int, seq_len: int) -> torch.Tensor:
     Returns:
         torch.Tensor: A tensor containing ALiBi to be added to attention scores.
     """
-    pass
+    fir = torch.arange(1, seq_len+1).unsqueeze(1)
+    sec = torch.arange(1, seq_len+1).unsqueeze(0)
+    dist =  (-abs(fir - sec).tril()).unsqueeze(0).float()
+    dist = dist - torch.transpose(dist, 1, 2)
+    answer = torch.clone(dist.expand(num_heads,seq_len,seq_len))
+    powers = -torch.linspace(8/num_heads, 8, num_heads)
+    m = torch.pow(2, powers)
+    answer *= m.view(-1,1,1)
+    return answer
+
 
 
 if __name__ == "__main__":
-    bias = compute_alibi(4, 4)
+    bias = compute_alibi(4, 5)
     print(bias)
--- Homework/03/gqa.py
+++ Homework/03/gqa.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 
 
 def scaled_dot_product_gqa(
@@ -20,4 +21,27 @@ def scaled_dot_product_gqa(
             - (Optional) Attention weights with shape [batch size; num heads; seq len; kv seq len].
                 Only returned if 'need_weights' is True.
     """
-    pass
+    num_heads = query.size(2)
+    num_kv_heads = key.size(2)
+    seq_len = query.size(1)
+    if num_heads < num_kv_heads:
+        raise ValueError
+    mul = num_heads//num_kv_heads
+    batch_size, kv_seq_len, num_kv_heads, hidden_dim = key.shape
+    A = torch.zeros((batch_size, num_heads, seq_len, kv_seq_len))
+    H = torch.zeros((batch_size, seq_len, num_heads, hidden_dim))
+    for k in range(mul):
+        sub_q = query[:,:,k::mul]
+        if is_causal:
+            tmp = torch.matmul(sub_q.transpose(1,2), key.transpose(1,2).transpose(2,3))/ hidden_dim ** 0.5
+            causal_mask = torch.triu(torch.ones(batch_size, num_kv_heads, seq_len, kv_seq_len), diagonal=1).bool()
+            tmp = tmp.masked_fill(causal_mask, float('-inf'))
+            attention_scores = F.softmax(tmp, dim=-1).tril()
+        else:
+            attention_scores = F.softmax(torch.matmul(sub_q.transpose(1,2), key.transpose(1,2).transpose(2,3)) / hidden_dim ** 0.5, dim=-1)
+        attention_output = torch.matmul(attention_scores, value.transpose(1,2))
+        H[:,:,k::mul,:] = attention_output.transpose(1,2).clone()
+        A[:,k::mul,:,:] = attention_scores.clone()
+    if need_weights:
+        return H, A
+    return H
